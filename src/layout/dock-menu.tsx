a8ba2a78155c6c33d6901d6a62e82ce92b25729a
Elizar Aitbek <elizar.aitbek@gmail.com>
2024-11-11 02:22:51 +0600

    feat: add nav dock

--- src/layout/dock-menu.tsx
+++ src/layout/dock-menu.tsx
@@ -1,3 +1,27 @@
+import { links } from '@/utils/constants'
+import { Link, useLocation } from 'react-router-dom'
+
 export default function DockMenu() {
-   return <div>dock-menu</div>
+   const location = useLocation()
+
+   return (
+      <div className="bg-secondary-bg fixed bottom-4 left-1/2 w-11/12 max-w-md -translate-x-1/2 transform rounded-[15px] border py-2">
+         <nav className="flex justify-around">
+            {links.map((link) => (
+               <Link
+                  key={link.path}
+                  to={link.path}
+                  className={`flex flex-col items-center gap-1 px-3 py-1 ${
+                     location.pathname === link.path
+                        ? 'text-text-light'
+                        : 'text-gray-500'
+                  } transition-colors`}
+               >
+                  <link.icon className="w-8" />
+                  <span className="text-xs">{link.label}</span>
+               </Link>
+            ))}
+         </nav>
+      </div>
+   )
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+import MainIcon from '../assets/icons/main-icon.svg?react'
+import RocketIcon from '../assets/icons/rocket-icon.svg?react'
+import RefreshIcon from '../assets/icons/refresh-icon.svg?react'
+import CatsIcon from '../assets/icons/cats-icon.svg?react'
+import InfoIcon from '../assets/icons/info-icon.svg?react'
+import WalletIcon from '../assets/icons/wallet-icon.svg?react'
+
+export const links = [
+   { path: '/', label: 'Main', icon: MainIcon },
+   { path: '/boosts', label: 'Boosts', icon: RocketIcon },
+   { path: '/tasks', label: 'Tasks', icon: RefreshIcon },
+   { path: '/frens', label: 'Frens', icon: CatsIcon },
+   { path: '/info', label: 'Info', icon: InfoIcon },
+   { path: '/wallet', label: 'Wallet', icon: WalletIcon },
+]
